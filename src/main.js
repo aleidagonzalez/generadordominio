e3d792061f664d790a05ae8f04bf06c72153eaf3
aleidagonzalez <aleida1983@hotmail.com>
2021-03-06 18:24:45 +0000

    nuevo

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronounWord = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let adjWord = adj[Math.floor(Math.random() * adj.length)];
+  let nounWord = noun[Math.floor(Math.random() * noun.length)];
+
+  let domain = pronounWord + adjWord + nounWord + ".";
+
+  console.log(domain);
 };
